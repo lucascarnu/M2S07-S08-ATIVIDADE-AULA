4b1eb5b6766b143412c05976a7f091c209b9a293
lucascarnu <lucascarnu@gmail.com>
2024-07-22 18:18:46 -0300

    Aula 2 Atividade 2 e 3 completas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 // Arquivo de rotas
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    //caminho para redirecionar para o componente, neste caso HomeComponent
+    path: 'home',
+    component: HomeComponent, // é importado o Homecomponent from './home/home.component';
+  },
+  {
+    path: '**', // geralmente se usa para ir para uma pagina not-found
+    redirectTo: 'home',
+  },
+];
